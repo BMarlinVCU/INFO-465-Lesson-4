8ded9868fd6351641463d60c3916c8ab3005ec5a
BMarlinVCU <bjmarlin@vcu.edu>
2026-09-18 00:22:57 +0000

    Pandas example

---
+++ dataframe.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import sqlite3
+
+conn=sqlite3.connect('project.db')
+#create a dataframe from the database
+df_weather=pd.read_sql_query("SELECT * FROM WEATHER", conn)
+print(df_weather.head())
+print(df_weather.describe())
+df_weather['time'] = pd.to_datetime(df_weather['time'])
+ax = df_weather.plot(x='time', y='temperature_c', kind='line')
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+plt.savefig('temperature_plot.png')
